ff65e5e14307cfedf691986b3e31f6fecb0747bd
Jason Wood <darimus@gmail.com>
2025-10-10 14:42:09 -0400

    Add glowing pipe head

--- src/main/java/com/dairymoose/biomech/armor/renderer/PipeMechHeadRenderer.java
+++ src/main/java/com/dairymoose/biomech/armor/renderer/PipeMechHeadRenderer.java
@@ -4,7 +4,9 @@ import com.dairymoose.biomech.BioMech;
 
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelib.rewrite.render.layer.AzAutoGlowingLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 public class PipeMechHeadRenderer extends AzArmorRenderer {
     private static final ResourceLocation GEO = ResourceLocation.fromNamespaceAndPath(
@@ -18,6 +20,6 @@ public class PipeMechHeadRenderer extends AzArmorRenderer {
     );
 
     public PipeMechHeadRenderer() {
-        super(AzArmorRendererConfig.builder(GEO, TEX).build());
+        super(AzArmorRendererConfig.builder(GEO, TEX).addRenderLayer(new AzAutoGlowingLayer<ItemStack>()).build());
     }
 }
