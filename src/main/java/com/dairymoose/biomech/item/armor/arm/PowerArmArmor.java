67cb312aa1fd6038239a24acb40b619d475db285
Jason Wood <darimus@gmail.com>
2025-10-11 18:32:26 -0400

    Enlarge optics unit

--- src/main/java/com/dairymoose/biomech/item/armor/arm/PowerArmArmor.java
+++ src/main/java/com/dairymoose/biomech/item/armor/arm/PowerArmArmor.java
@@ -18,7 +18,7 @@ public abstract class PowerArmArmor extends ArmorBase {
 
 	public PowerArmArmor(ArmorMaterial p_40386_, Type p_266831_, Properties p_40388_) {
 		super(p_40386_, p_266831_, p_40388_);
-		this.suitEnergy = 0;
+		this.suitEnergy = 2;
 		this.hidePlayerModel = true;
 	}
 
