a4c710bb94a97428708d2a97396bdd5f71ff51a1
Jason Wood <darimus@gmail.com>
2025-10-13 13:43:43 -0400

    Night vision glow

--- src/main/java/com/dairymoose/biomech/armor/renderer/NightVisionVisorRenderer.java
+++ src/main/java/com/dairymoose/biomech/armor/renderer/NightVisionVisorRenderer.java
@@ -4,7 +4,9 @@ import com.dairymoose.biomech.BioMech;
 
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRenderer;
 import mod.azure.azurelib.rewrite.render.armor.AzArmorRendererConfig;
+import mod.azure.azurelib.rewrite.render.layer.AzAutoGlowingLayer;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
 
 public class NightVisionVisorRenderer extends AzArmorRenderer {
     private static final ResourceLocation GEO = ResourceLocation.fromNamespaceAndPath(
@@ -18,6 +20,8 @@ public class NightVisionVisorRenderer extends AzArmorRenderer {
     );
 
     public NightVisionVisorRenderer() {
-        super(AzArmorRendererConfig.builder(GEO, TEX).build());
+        super(AzArmorRendererConfig.builder(GEO, TEX)
+        		.addRenderLayer(new AzAutoGlowingLayer<ItemStack>())
+        		.build());
     }
 }
