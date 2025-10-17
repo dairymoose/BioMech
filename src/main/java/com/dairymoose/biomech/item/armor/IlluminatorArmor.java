f275c50cb316cc1792ca0295a5af33b8dd433f4e
Jason Wood <darimus@gmail.com>
2025-10-16 21:32:29 -0400

    Update syntax

--- src/main/java/com/dairymoose/biomech/item/armor/IlluminatorArmor.java
+++ src/main/java/com/dairymoose/biomech/item/armor/IlluminatorArmor.java
@@ -63,7 +63,7 @@ public class IlluminatorArmor extends ArmorBase {
 						status.toggledOn = !status.toggledOn;
 					}
 				} else {
-					status.toggledOn = bonusData == 1 ? true : false;
+					status.toggledOn = (bonusData == 1 ? true : false);
 				}
 			}
 			this.sendHotkeyToServer(player, keyIsDown, status.toggledOn ? 1 : 0, BroadcastType.SEND_TO_ALL_CLIENTS, serverOriginator);
