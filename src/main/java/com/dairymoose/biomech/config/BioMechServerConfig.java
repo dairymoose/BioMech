cb654ad8f4a0921364c7e8c2c94c59cf1e20e764
Jason Wood <darimus@gmail.com>
2025-10-20 01:39:35 -0400

    Increase gatling damage by 1

--- src/main/java/com/dairymoose/biomech/config/BioMechServerConfig.java
+++ src/main/java/com/dairymoose/biomech/config/BioMechServerConfig.java
@@ -12,7 +12,7 @@ public class BioMechServerConfig {
 	public BioMechServerConfig(ForgeConfigSpec.Builder builder) {		
 		builder.push("hand_items");
 		this.gatlingMinFalloffFactor = builder.comment("gatlingMinFalloffFactor").translation("config.biomech.gatlingMinFalloffFactor").defineInRange("gatlingMinFalloffFactor", 0.5, 0.0, 1.0);
-		this.gatlingDamage = builder.comment("gatlingDamage").translation("config.biomech.gatlingDamage").define("gatlingDamage", 25.0);
+		this.gatlingDamage = builder.comment("gatlingDamage").translation("config.biomech.gatlingDamage").define("gatlingDamage", 26.0);
 		this.gatlingEnergyPerSec = builder.comment("gatlingEnergyPerSec").translation("config.biomech.gatlingEnergyPerSec").define("gatlingEnergyPerSec", 15.0);
 		builder.pop();
 	}
